dcdd4f2c6b9b40a42f76a9073d7241aa804b5abc
Omsubhra Singha <124859628+omsusi@users.noreply.github.com>
2025-07-15 13:07:36 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from 'tailwindcss'
-import autoprefixer from 'autoprefixer'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), autoprefixer()],
+  plugins: [react()], // Only react() plugin here
 })
