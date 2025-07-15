4a96e2df6212e03cb2f42875fe284d4deda3c9d7
Omsubhra Singha <124859628+omsusi@users.noreply.github.com>
2025-07-15 14:17:40 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './components/header/Header'
 import Home from './components/home/Home'
